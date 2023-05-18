f8866d3b2997677e32f0dea185f9e1234e92c614
laurenyj <laurenyj@icloud.com>
2023-05-17 19:23:44 -0500

    Clean up QuizHeader spec

--- src/components/tests/QuizHeader.spec.js
+++ src/components/tests/QuizHeader.spec.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react-native';
+import { render } from '@testing-library/react-native';
 import '@testing-library/jest-dom'
 
 import QuizHeader from '../QuizHeader';
 
 describe('QuizHeader', () => {
-  it('should render successfully', () => {
-    render(<QuizHeader />);
-    expect(screen.getByText('QUIZ')).toBeVisible();
-    expect(screen.getByText("Take our quiz to find out where you should go for dinner.")).toBeVisible();
+  it('should render successfully with text', () => {
+    const { getByText } = render(<QuizHeader />);
+    expect(getByText('QUIZ')).toBeTruthy();
+    expect(getByText('Take our quiz to find out where you should go for dinner.')).toBeTruthy();
   });
 });
